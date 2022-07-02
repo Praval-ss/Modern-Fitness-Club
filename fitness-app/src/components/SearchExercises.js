130a05a9ba41ed39cc16467bd75f0ef9839dedae
Praval-ss <pravalgopalp2@gmail.com>
2022-07-03 03:23:16 +0530

    First commit

---
+++ fitness-app/src/components/SearchExercises.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+
+const SearchExercises = () => {
+  return (
+    <Stack alignItems="center" mt="37px"
+    justifyContent="center" p="20px">
+        <Typography fontWeight={700} sx={{
+            fontSize: {lg: '44px', xs: '30px'}
+        }} mb="50px" textAlign="center">
+            Awesome Excercises You <br/> Should Know
+        </Typography>
+        <Box position ="relative" mb="72px">
+            <TextField 
+            sx={{
+                input: {fontWeight: '100', border: 'none', borderRadius: '4px'},
+                width:{lg: '1170px', xs: '350px'},
+                backgroundColor:"#fff",
+                borderRadius: '40px'
+            }}
+                height="76px"
+                value=""
+                onChange={(e)=>{}}
+                placeholder="Search Exercises"
+                type="Text"
+            />
+        </Box>
+    </Stack>
+  )
+}
+
+export default SearchExercises
