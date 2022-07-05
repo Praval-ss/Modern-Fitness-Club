9289ca2a7ede000031a251d3ad6ab8cab43a16ef
Praval-ss <pravalgopalp2@gmail.com>
2022-07-05 18:54:13 +0530

    created new exercise detail page,

--- fitness-app/src/utils/fetchData.js
+++ fitness-app/src/utils/fetchData.js
@@ -1,15 +1,16 @@
 export const exerciseOptions = {
     method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+    // url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
     }
   };
 
 export const fetchData =async (url,options) =>{
     const response =await fetch(url,options);
     const data = await response.json();
+    console.log(data,"hello");
 
     return data;
 }
